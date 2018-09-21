9bcc4c76c15d5b387f6c52350f4f83be2d3d05f5
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-09-21 13:03:43 -0400

    built survey routes

--- routes/survey.js
+++ routes/survey.js
@@ -11,15 +11,47 @@ router.get('/', (req,res) => {
 })
 
 //NEW, RENDER NEW FORM
-
+router.get('/new', (req,res) => {
+    res.render('surveys/new')
+})
 //SHOW, SHOW ONE
+router.get('/:id', (req,res) => {
+    Survey.findById(req.params.id)
+    .then((survey) => {
+        res.render('surveys/show', {survey})
+    })
+})
 
 //EDIT, RENDER EDIT FORM
+router.get('/:id/edit', (req,res) => {
+    Survey.findById(req.params.id)
+    .then((survey) => {
+        res.render('surveys/edit', {survey})
+    })
+})
 
 //CREATE
+router.post('/', (req,res) => {
+    Survey.create(req.body)
+    .then((survey) => {
+        res.redirect(`surveys/${survey._id}`)
+    })
+})
 
 //UPDATE
+router.put('/:id', (req, res) => {
+    Survey.findByIdAndUpdate(req.params.id, req.body)
+    .then((survey) => {
+        res.redirect(`surveys/${survey._id}`)
+    })
+})
 
 //DELETE
+router.delete('/:id', (req, res) => {
+    Survey.findByIdAndRemove(req.params.id)
+    .then(() => {
+        res.redirect('/surveys')
+    })
+})
 
 module.exports = router
