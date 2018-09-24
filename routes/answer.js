e6d86f040205325f3dcdafabadb13f462503967b
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-09-24 10:22:08 -0400

    created third model edit and js files

--- routes/answer.js
+++ routes/answer.js
@@ -2,4 +2,14 @@ const express = require('express')
 const router = express.Router( {mergeParams: true} )
 const { Survey, Question, Answer } = require('../db/schema')
 
+//EDIT, RENDER EDIT FORM
+router.get('/:id/edit', (req,res) => {
+    Survey.findById(req.params.id)
+    .then((survey) => {
+        const questions = survey.questions
+        const answers = survey.question.answers
+        res.render('answers/edit')
+    })
+})
+
 module.exports = router
